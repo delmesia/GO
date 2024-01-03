e08e9921c6679ceb71d540fa3bb9df9ac4f2d2f7
delmesia <wendellsmesia@gmail.com>
2024-01-03 20:32:03 +0800

    hello world and integers

---
+++ helloworld/hello.go
@@ -0,0 +1,34 @@
+package main
+
+const (
+	spanish = "Spanish"
+	french  = "French"
+	english = "English"
+
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case "French":
+		prefix = frenchHelloPrefix
+	case "Spanish":
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
+
+func main() {
+
+}
